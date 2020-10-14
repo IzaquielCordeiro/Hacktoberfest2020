9a07817480f8d75fba22596dd69f42ff1732abde
Dacinho <dacio.bezerra@ccc.ufcg.edu.br>
2020-10-14 18:03:24 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Add(new DateTime(2011, 4, 25)));
+            Console.WriteLine(ColorCode("White"));
+        }
+
+        private static int ColorCode(string color)
+        {
+            return Array.IndexOf(Colors(), color);
+        }
+
+        private static string[] Colors()
+        {
+            string[] arr = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"};
+            return arr;
+        }
+        private static DateTime Add(DateTime moment){
+            DateTime ad = moment.AddSeconds(1000000000);
+            return ad;
+        }   
+    }
+}
